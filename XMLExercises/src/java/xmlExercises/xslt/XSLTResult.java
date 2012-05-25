6b24f6b7425777411fc39565f490de46eb589449
MatousJobanek <matousjobanek@gmail.com>
2012-05-25 13:47:08 +0200

    preparation for xsltResult

---
+++ XMLExercises/src/java/xmlExercises/xslt/XSLTResult.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlExercises.xslt;
+
+/**
+ *
+ * @author Matous Jobanek
+ */
+public class XSLTResult {
+    
+
+    private boolean successful;
+
+    private String xsl;
+
+    private String gti ;
+    
+//    private String xsl;
+
+    private String transformed;
+    
+}
