1732a22111319a9f9836ed01c214b6edea6eb335
Alexandr Toptygin <toptygin@gmail.com>
2012-05-28 21:49:26 +0200

    Added temp file class to the main project.

---
+++ XMLExercises/src/java/xmlExercises/SchemaEvaluator.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlExercises;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+
+/**
+ *
+ * @author Alexandr Toptygin
+ */
+public class SchemaEvaluator implements Evaluator{
+    String victoryMessage = "Winrar is You!";
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    TempFileHandler tfh = null;
+    
+    // Recieves directory name, as tarballing would be unnecessary
+    public SchemaEvaluator(String tempDir) throws IOException{
+        tfh = new TempFileHandler(tempDir, "xsd");
+        factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(true);
+        factory.setAttribute(
+          "http://java.sun.com/xml/jaxp/properties/schemaLanguage", 
+          "http://www.w3.org/2001/XMLSchema");    
+    }
+    private String checkWrong(String dirName, Document doc){
+        File TestDir = new File(dirName);
+        String result = victoryMessage;
+        boolean CorrectlyInvalid = true;
+        String[] dirList = TestDir.list();
+        for (int i = 0; i < dirList.length; i++)  {
+        try{
+                if((dirList[i].split("_"))[0].equalsIgnoreCase("err")){
+                    DocumentBuilder parser = factory.newDocumentBuilder();
+                    CorrectlyInvalid = false;
+                    doc = parser.parse(dirName.concat(dirList[i]));                    
+                }
+           }
+        catch (ParserConfigurationException e){
+             result = e.getMessage();
+           }
+        catch (SAXException e){
+             CorrectlyInvalid = true;
+           }
+        catch (IOException e){
+             result = "Something's wrong. Call an admin!";
+           }
+         }
+        if(CorrectlyInvalid) return victoryMessage;
+        return result;
+
+    }
+    
+    
+    private String checkCorrect(String dirName, Document doc){        
+        File TestDir = new File(dirName);
+        String result = victoryMessage;
+        String[] dirList = TestDir.list();
+        for (int i = 0; i < dirList.length; i++)  {
+        try{
+                System.out.println("lol"+i+"wat"+(dirList[i].split("_")).length);
+                if((dirList[i].split("_"))[0].equalsIgnoreCase("cor")){
+                    DocumentBuilder parser = factory.newDocumentBuilder();
+                    doc = parser.parse(dirName.concat(dirList[i]));
+                }
+           }
+        catch (ParserConfigurationException e){
+             result = e.getMessage();
+           }
+        catch (SAXException e){
+             result = ("Parsing XML failed due to a " + e.getClass().getName() + ":\n" +e.getMessage());
+           }
+        catch (IOException e){
+             result = "Something's wrong. Call an admin!";
+           }
+         }
+        return result;
+    }
+    
+    @Override
+    public String eval(String expresion, String dirName) throws SyntaxErorException, IOException, OverflowException{
+  
+        String result;
+        
+        String path = tfh.addFile(expresion);
+        
+        factory.setAttribute(
+          "http://java.sun.com/xml/jaxp/properties/schemaSource",
+          path);
+        Document doc = null;
+        
+        result = checkCorrect(dirName, doc);        
+        if(result.equals(victoryMessage)) result = checkWrong(dirName, doc);
+        
+        tfh.deleteFile(path);
+        return result;
+    }
+    
+    @Override
+    public boolean compare(String result1, String result2){
+        return result1.equals(result2);
+    }
+}
